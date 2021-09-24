cf3c5dceaac435b70f48197e2e5fe8fc1b532020
tlgkien <tlgkien@pop-os.localdomain>
2021-09-24 07:29:34 +0700

    fix the contract and add README.md

---
+++ README.md
@@ -0,0 +1,173 @@
+NCD-Demo
+==================
+
+Vietnamese gambling game: 
+Gourd-Fish-Shrimp-Crab On NEAR
+====================
+
+## Why dice
+
+
+## How to play
+
+Instead of showing one to six pips, the sides of the dice have pictures of a fish, a prawn, a crab, a rooster, a calabash gourd,and a stag.We have 3 dice.
+Players place wagers on a board that has the six pictures
+The bettor will get their money back plus the corresponding amount of profit if the bet is correct
+If two (or three) dice correspond with a bet, the bettor receives tow (or three) times their money
+For instance, if one were to place $3 on fish, and the dealer rolls 1 fish, 1 crab and 1 stag, then the bettor would receive $3 while keeping the $3 they had bet
+
+
+About Contract
+====================
+It's need to be mentioned that it is a pure dapp project, which means there is no centralized backend nor data server, all persistent information is stored and mananged on NEAR chain by a contract.
+
+## Contract Structure
+
+```rust
+// information about bets allocation
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct BetInfo {
+    pub fish: u128, 
+    pub prawn: u128, 
+    pub crab: u128, 
+    pub rooster: u128, 
+    pub gourd: u128, 
+    pub stag: u128,
+}
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct GourdCrab {
+    pub owner_id: AccountId,
+    pub accounts: UnorderedMap<AccountId, Balance>, // balance of user
+    pub bets: UnorderedMap<AccountId,BetInfo>,      // bets in the room
+    pub bet_level: Balance, 
+}
+```
+
+## Contract Interface
+
+
+```rust
+//****************/
+//***** INIT *****/
+//****************/
+
+/// initialization of this contract
+#[init]
+    pub fn new(
+        owner_id: AccountId,
+        bet_level: U128, 
+    ) -> Self;
+
+
+//***************************/
+//***** OWNER FUNCTIONS *****/
+//***************************/
+
+pub fn set_bet_level(&mut self, bet_level: U128);
+
+pub fn deposit(&mut self); 
+
+pub fn betting(
+    &mut self, 
+    num_fish: u128, 
+    num_prawn: u128,
+    num_crab: u128,
+    num_rooster: u128, 
+    num_gourd: u128,
+    num_stag: u128);
+
+pub fn cancel_betting(&mut self);
+
+// roll the dice and calculate the payoff
+pub fn rolling(&mut self);
+
+// get account's balance
+pub fn get_balance(&self, account_id: AccountId) -> u128;
+
+```
+
+Quick Start
+===========
+
+To run this project locally:
+
+1. Prerequisites: Make sure you've installed [Node.js] ≥ 12
+
+Now you'll have a local development environment backed by the NEAR TestNet!
+
+Go ahead and play with the app and the code. As you make code changes, the app will automatically reload.
+
+
+Exploring The Code
+==================
+
+The "contract" code lives in the `/src` folder. See the README there for more info.
+
+Deploy
+======
+
+Every smart contract in NEAR has its [own associated account][NEAR accounts]. When you run `yarn dev`, your smart contract gets deployed to the live NEAR TestNet with a throwaway account. When you're ready to make it permanent, here's how.
+
+
+Step 0: Install near-cli (optional)
+-------------------------------------
+
+[near-cli] is a command line interface (CLI) for interacting with the NEAR blockchain. It was installed to the local `node_modules` folder when you ran `yarn install`, but for best ergonomics you may want to install it globally:
+
+    yarn install --global near-cli
+
+Or, if you'd rather use the locally-installed version, you can prefix all `near` commands with `npx`
+
+Ensure that it's installed with `near --version` (or `npx near --version`)
+
+How to run
+==========
+After you clone the project, go to the project folder
+Step 1: Login
+-------------
+
+Each account on NEAR can have at most one contract deployed to it. If you've already created an account such as `your-name.testnet`, you can deploy your contract to `NCD-GroupA-Demo.your-name.testnet`. Assuming you've already created an account on [NEAR Wallet], here's how to create `NCD-GroupA-Demo.your-name.testnet`:
+
+1. Authorize NEAR CLI, following the commands it gives you:
+
+      `near login`
+
+2. Create a subaccount (replace `YOUR-NAME` below with your actual account name):
+
+      `near create-account NCD-GroupA-Demo.YOUR-NAME.testnet --masterAccount YOUR-NAME.testnet`
+
+3. Export account to variable
+      `export ACCOUNT_ID=<YOUR_ACCOUNT_ID>`
+
+Step 2: build and testing
+---------------------------------
+
+    `./build.sh`
+    `./test.sh`
+
+
+Step 3: deploy!
+---------------
+
+    `./deploy.sh`
+
+Step 4: Call contract from Near CLI
+
+    `./call.sh <METHOD_NAME> <ARGs>`
+
+Troubleshooting
+===============
+
+On Windows, if you're seeing an error containing `EPERM` it may be related to spaces in your path. Please see [this issue](https://github.com/zkat/npx/issues/209) for more details.
+
+
+  [Vue]: https://vuejs.org/
+  [create-near-app]: https://github.com/near/create-near-app
+  [Node.js]: https://nodejs.org/en/download/package-manager/
+  [jest]: https://jestjs.io/
+  [NEAR accounts]: https://docs.near.org/docs/concepts/account
+  [NEAR Wallet]: https://wallet.testnet.near.org/
+  [near-cli]: https://github.com/near/near-cli
+  [gh-pages]: https://github.com/tschaub/gh-pages
--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,28 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::wee_alloc;
-use near_sdk::serde::{Deserialize, Serialize};
+// use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{
-    env, near_bindgen,AccountId, Balance, PublicKey, Promise,
-    collections::{ Vector, UnorderedMap },
-    json_types::{ U128, Base58PublicKey },
+    env, near_bindgen,AccountId, Balance,
+    collections::{ UnorderedMap },
+    json_types::{ U128},
 };
 
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-const ONE_NEAR:u128 = 1_000_000_000_000_000_000_000_000;
-const PROB:u8 = 128;
-
 
 // information about bets allocation
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct BetInfo {
-    pub user: AccountId,  
-    pub bet: UnorderedMap<String,u8>,
+    pub fish: u128, 
+    pub prawn: u128, 
+    pub crab: u128, 
+    pub rooster: u128, 
+    pub gourd: u128, 
+    pub stag: u128,
 }
 
-impl BetInfo{
-    #[init]
-    pub fn new(
-        num_fish: u8, 
-        num_prawn: u8, 
-        num_crab: u8, 
-        num_rooster: u8, 
-        num_gourd: u8, 
-        num_stag: u8,
-    ) -> Self{
-
-    }
-}
 
 
 
@@ -43,7 +31,7 @@ impl BetInfo{
 pub struct GourdCrab {
     pub owner_id: AccountId,
     pub accounts: UnorderedMap<AccountId, Balance>, // balance of user
-    pub bets: Vector<BetInfo>,                      // bets in the room
+    pub bets: UnorderedMap<AccountId,BetInfo>,      // bets in the room
     pub bet_level: Balance, 
 }
 
@@ -65,11 +53,14 @@ impl GourdCrab {
         Self {
             owner_id,
             accounts:UnorderedMap::new(b"a".to_vec()),
-            bets:Vector::new(b"b".to_vec()),
+            bets:UnorderedMap::new(b"b".to_vec()),
             bet_level: bet_level.into(),
         }
     }
 
+    pub fn set_bet_level(&mut self, bet_level: U128){
+        self.bet_level = bet_level.into();
+    }
 
     #[payable]
     pub fn deposit(&mut self) {
@@ -79,29 +70,72 @@ impl GourdCrab {
         balance = balance + deposit;
         self.accounts.insert(&account_id, &balance);
     }
-
-    pub fn betting(&mut self, num_fish: u8, num_prawn: u8, num_crab: u8, num_rooster: u8, num_gourd: u8, num_stag: u8){
+    
+    pub fn betting(
+        &mut self, 
+        num_fish: u128, 
+        num_prawn: u128, 
+        num_crab: u128, 
+        num_rooster: u128, 
+        num_gourd: u128, 
+        num_stag: u128
+    ){
         let account_id = env::signer_account_id();
         let mut balance = self.accounts.get(&account_id).unwrap_or(0);
-        let total_bet = (num_fish + num_prawn + num_crab + num_rooster + num_gourd + num_stag);
-        assert!(balance > total_bet * ONE_NEAR, "Insufficient balance to bet");
-        balance = balance - total_bet * ONE_NEAR;
+        let total_bet = num_fish + num_prawn + num_crab + num_rooster + num_gourd + num_stag;
+        assert!(balance > total_bet * self.bet_level, "Insufficient balance to bet");
+        balance = balance - total_bet * self.bet_level;
+        self.accounts.insert(&account_id, &balance);
 
-
-
-
-        self.bet.push(&bet);
+        // thêm bet vào vector
+        let bet = BetInfo{
+            fish: num_fish, 
+            prawn: num_prawn, 
+            crab: num_crab, 
+            rooster: num_rooster, 
+            gourd: num_gourd, 
+            stag: num_stag,
+        };
+        self.bets.insert(&account_id,&bet);
     }
 
-    pub fn play(){
+    pub fn cancel_betting(&mut self){
 
     }
 
+    pub fn rolling(&mut self)->[u128;6]{
+        // rolling
+        let _rand1: u8 = *env::random_seed().get(0).unwrap() % 6;
+        let _rand2: u8 = *env::random_seed().get(0).unwrap() % 6;
+        let _rand3: u8 = *env::random_seed().get(0).unwrap() % 6;
 
+        let mut result: [u128;6] = [0,0,0,0,0,0];
+        result[_rand1 as usize] += 1;
+        result[_rand2 as usize] += 1;
+        result[_rand3 as usize] += 1;
+
+        for i in result{
+            println!("The result of rolling: {}", i);
+        }
+        
+        // check the result
+        for account_id in self.bets.keys() {
+            let mut balance = self.accounts.get(&account_id).unwrap_or(0);
+            let _tmp = self.bets.get(&account_id).unwrap();
+            let profit :u128 = result[0] as u128 * _tmp.fish + result[1] as u128 *_tmp.prawn + result[2] as u128 * _tmp.crab + result[3] as u128 * _tmp.rooster + result[4] as u128 * _tmp.gourd + result[5] as u128 * _tmp.stag;
+            balance = balance + profit * self.bet_level;
+
+            self.accounts.insert(&account_id, &balance);            
+        }
+
+        self.bets.clear();
+        result
+    }
     
-    pub fn get_balance(&self, account_id: AccountId) -> U128 {
-        self.accounts.get(&account_id).unwrap_or(0).into()
+    pub fn get_balance(&self, account_id: AccountId) -> u128 {
+        self.accounts.get(&account_id).unwrap_or(0)
     }
+
 }
 
 
@@ -109,37 +143,60 @@ impl GourdCrab {
 
 
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use near_sdk::MockedBlockchain;
-//     use near_sdk::{testing_env, VMContext};
-
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use near_sdk::MockedBlockchain;
+    use near_sdk::{testing_env};
+    mod test_utils;
+    use test_utils::*;
   
-//     fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
-//         VMContext {
-//             current_account_id: "alice.testnet".to_string(),
-//             signer_account_id: "robert.testnet".to_string(),
-//             signer_account_pk: vec![0, 1, 2],
-//             predecessor_account_id: "jane.testnet".to_string(),
-//             input,
-//             block_index: 0,
-//             block_timestamp: 0,
-//             account_balance: 0,
-//             account_locked_balance: 0,
-//             storage_usage: 0,
-//             attached_deposit: 0,
-//             prepaid_gas: 10u64.pow(18),
-//             random_seed: vec![0, 1, 2],
-//             is_view,
-//             output_data_receivers: vec![],
-//             epoch_height: 19,
-//         }
-//     }
+    fn account_casino() -> String {
+        "casino".to_string()
+    }
 
+    fn accunt_bob() -> String {
+        "bob".to_string()
+    }
 
-//     #[test]
-//     fn increment() {
+    fn accunt_alice() -> String {
+        "alice".to_string()
+    }
 
-//     }
-// }
+    const BET_LEVEL:u128 = 1_000_000_000_000_000_000_000_000;
+
+    fn start_machine() -> GourdCrab {
+        // initialize contract and deposit jackpod with 100 NEAR
+        let context = VMContextBuilder::new()
+            .current_account_id(account_casino())
+            .predecessor_account_id(account_casino())
+            .signer_account_id(account_casino())
+            .attached_deposit(BET_LEVEL * 100)
+            .finish();
+        testing_env!(context.clone());
+        let mut contract = GourdCrab::new(account_casino(), U128(BET_LEVEL));
+        contract
+    }
+
+    #[test]
+    fn test_rolling_successfully() {
+        let mut contract = start_machine();
+
+        
+
+        let context = VMContextBuilder::new()
+            .predecessor_account_id(accunt_bob())
+            .signer_account_id(accunt_bob())
+            .attached_deposit(10*BET_LEVEL)
+            .finish();
+
+        testing_env!(context.clone());    
+
+        contract.deposit();
+        assert_eq!(contract.get_balance(accunt_bob()),BET_LEVEL*10);
+
+        contract.betting(2,0,0,0,0,0);
+        contract.rolling();
+        
+    }
+}
